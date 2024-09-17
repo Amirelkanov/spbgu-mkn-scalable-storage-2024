2d20ef5e5513622f4433604d032f6b66ad83865f
AmEl <amirelkanov0504@gmail.com>
2024-09-17 16:13:05 +0300

    Added mutex lock

--- practice0/main.go
+++ practice0/main.go
@@ -2,36 +2,53 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
 func main() {
-	b := [100]byte{}
+	var b [100]byte // Массив для изменения
+	// Мьютекс для синхронизации (наверное, можно и каналом, заведя какой-нибудь ch bool для уверенности в том,
+	// что только одна горутина владеет критической секцией, но зачем?)
+	var mu sync.Mutex
 
 	go func() {
-		filler(b[:len(b)/2], '0', '1')
-		time.Sleep(time.Second)
+		for {
+			mu.Lock()
+			filler(b[:len(b)/2], '0', '1')
+			mu.Unlock()
+			time.Sleep(1 * time.Second)
+		}
 	}()
 
 	go func() {
-		filler(b[:len(b)/2], 'X', 'Y')
-		time.Sleep(time.Second)
+		for {
+			mu.Lock()
+			filler(b[len(b)/2:], 'X', 'Y')
+			mu.Unlock()
+			time.Sleep(1 * time.Second)
+		}
 	}()
 
 	go func() {
-		fmt.Println(string(b[:]))
-		time.Sleep(time.Second)
+		for {
+			mu.Lock()
+			fmt.Println(string(b[:]))
+			mu.Unlock()
+			time.Sleep(1 * time.Second)
+		}
 	}()
 
-	select {}
+	for {
+	}
 }
 
 func filler(b []byte, ifzero byte, ifnot byte) {
-	for i := 0; i < 2; i++ {
-		if i == 0 {
+	for i := 0; i < len(b); i++ {
+		if rand.Int()%2 == 0 {
 			b[i] = ifzero
-		}
-		if i == 1 {
+		} else {
 			b[i] = ifnot
 		}
 	}
